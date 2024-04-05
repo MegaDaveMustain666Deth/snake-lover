0dec874a8a114254cdc787c2626acc9915468da4
mustaindave1992@gmail.com <129941509+MegaDaveMustain666Deth@users.noreply.github.com>
2024-04-05 22:49:10 +0500

    Commit to errors doesnt be

---
+++ Assets/Client/Scripts/Snake.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    public void Move(Vector2 direction)
+    {
+        
+    }
+
+    
+}
