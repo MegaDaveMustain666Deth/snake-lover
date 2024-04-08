c9180a1db54992e777ae3416e9490225bfbbf984
mustaindave1992@gmail.com <129941509+MegaDaveMustain666Deth@users.noreply.github.com>
2024-04-08 22:38:41 +0500

    LevelGenertor

--- Assets/Client/Scripts/LevelGenerator.cs
+++ Assets/Client/Scripts/LevelGenerator.cs
@@ -5,15 +5,44 @@ public class LevelGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject[] _tileGrounds;
     [SerializeField] private int _tileLength;
+    [SerializeField] private Transform _target;
+    [SerializeField] private int _amoutTileForAddingTile;
 
-    private List<GameObject> _currentTileGrounds = new List<GameObject>();
-    private Vector3 _nextSpawn = Vector3.zero;
+    private Queue<GameObject> _currentTileGrounds = new Queue<GameObject>();
+    private Vector3 _nextSpawn;
+    private float _deleteTile;
 
-    [ContextMenu("Add tale")]
-    private void AddTale()
+    private void Start()
+    {
+        _nextSpawn = new Vector3(0, 0, -34);
+        _currentTileGrounds.Clear();
+        for(int i = 0; i < _amoutTileForAddingTile; i++)
+        {
+            AddTile();
+        }
+    }
+
+    [ContextMenu("Add tile")]
+    private void AddTile()
     {
         int indexTale = Random.Range(0, _tileGrounds.Length);
-        _currentTileGrounds.Add(Instantiate(_tileGrounds[indexTale], _nextSpawn, Quaternion.identity));
+        _currentTileGrounds.Enqueue(Instantiate(_tileGrounds[indexTale], _nextSpawn, Quaternion.identity));
         _nextSpawn.z += _tileLength;
+        _deleteTile = _nextSpawn.z - (_tileLength * _amoutTileForAddingTile) + 34;
+    }
+
+    [ContextMenu("Add tddile")]
+    private void DeletePreviousTile()
+    {
+        Destroy(_currentTileGrounds.Dequeue());
+    }
+
+    private void Update()
+    {
+        if(_target.position.z > _deleteTile)
+        {
+            AddTile();
+            DeletePreviousTile();
+        }
     }
 }
