3df84c7f483e1064518b0a74372cc71e37020477
mustaindave1992@gmail.com <129941509+MegaDaveMustain666Deth@users.noreply.github.com>
2024-04-10 20:31:30 +0500

    ffffffffffffff

--- Assets/Client/Scripts/LevelGenerator.cs
+++ Assets/Client/Scripts/LevelGenerator.cs
@@ -1,24 +1,49 @@
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+
     [SerializeField] private GameObject[] _tileGrounds;
+    [SerializeField] private GameObject[] _backgroundObjects;
     [SerializeField] private int _tileLength;
-    [SerializeField] private Transform _target;
-    [SerializeField] private int _amoutTileForAddingTile;
+    [SerializeField] private int _amoutTileForAdding;
+    [SerializeField] private int _amoutObjectForAdding;
     [SerializeField] private Transform _startSpawn;
+    [SerializeField] private float _minDistanceBetweenObjects;
+    [SerializeField] private float _maxDistanceBetweenObjects;
+    [SerializeField] private Transform _startSpawnObjects;
 
     private Queue<GameObject> _currentTileGrounds = new Queue<GameObject>();
+    private Queue<GameObject> _currentBackgroundObjects = new Queue<GameObject>();
     private float _deleteTile;
+    private float _deleteObject;
+    private Transform _player;
 
     private void Start()
-    {;
+    {
+        _player = Snake.Instance.transform;
         _currentTileGrounds.Clear();
-        for(int i = 0; i < _amoutTileForAddingTile; i++)
+        _currentBackgroundObjects.Clear();
+        for(int i = 0; i < _amoutTileForAdding; i++)
         {
             AddTile();
         }
+        for(int i = 0; i < _amoutObjectForAdding; i++)
+        {
+            AddBackgroundObjects();
+        }
+    }
+
+    [ContextMenu("Add obj")]
+    private void AddBackgroundObjects()
+    {
+        float spawnObj = Random.Range(_minDistanceBetweenObjects, _maxDistanceBetweenObjects);
+        int indexTale = Random.Range(0, _backgroundObjects.Length);
+        _currentBackgroundObjects.Enqueue(Instantiate(_backgroundObjects[indexTale], _startSpawnObjects.position, Quaternion.identity));
+        _startSpawnObjects.position += new Vector3(0, 0, spawnObj);
+        _deleteObject = _startSpawnObjects.position.z - (spawnObj * _amoutObjectForAdding) + _minDistanceBetweenObjects;
     }
 
     [ContextMenu("Add tile")]
@@ -27,7 +52,7 @@ public class LevelGenerator : MonoBehaviour
         int indexTale = Random.Range(0, _tileGrounds.Length);
         _currentTileGrounds.Enqueue(Instantiate(_tileGrounds[indexTale], _startSpawn.position, Quaternion.identity));
         _startSpawn.position += new Vector3(0, 0, _tileLength);
-        _deleteTile = _startSpawn.position.z - (_tileLength * _amoutTileForAddingTile) + 34;
+        _deleteTile = _startSpawn.position.z - (_tileLength * _amoutTileForAdding) + 34;
     }
 
     [ContextMenu("Add tddile")]
@@ -36,12 +61,22 @@ public class LevelGenerator : MonoBehaviour
         Destroy(_currentTileGrounds.Dequeue());
     }
 
+    private void DeletePreviousObj()
+    {
+        Destroy(_currentBackgroundObjects.Dequeue());
+    }
+
     private void Update()
     {
-        if(_target.position.z > _deleteTile)
+        if(_player.position.z > _deleteTile)
         {
             AddTile();
             DeletePreviousTile();
+        }
+        if(_player.position.z > _deleteObject)
+        {
+            AddBackgroundObjects();
+            DeletePreviousObj();
         }
     }
 }
